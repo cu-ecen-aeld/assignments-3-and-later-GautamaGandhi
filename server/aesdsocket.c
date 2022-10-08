3bd05471d63778339172682c3ab9770ee1af267d
GautamaGandhi <gaga4723@colorado.edu>
2022-10-08 17:00:12 -0600

    Threading initial commit

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -23,6 +23,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 #define BACKLOG 10 // Setting 10 as the connection request limit while listening
 #define INITIAL_BUFFER_SIZE 1024 // Buffer size for receive and storage buffers
@@ -31,9 +32,11 @@
 // Global Variables
 int socketfd; // Socket File Descriptor
 int testfile_fd; // Testfile File Descriptor
-int new_fd; // File Descriptor used to receive and send data
-char *storage_buffer; // Pointer to storage buffer
+// int new_fd; // File Descriptor used to receive and send data
+//char *storage_buffer; // Pointer to storage buffer
 
+
+pthread_mutex_t mutex;
 
 // Signal handler for SIGINT and SIGTERM signals
 void sig_handler(int signum)
@@ -43,20 +46,17 @@ void sig_handler(int signum)
     else if (signum == SIGTERM)
         syslog(LOG_DEBUG, "Caught signal SIGTERM, exiting");
 
-    // Freeing storage buffer allocated memory 
-    // free(storage_buffer);
-
     //Closing socketfd FD
     int status = close(socketfd);
     if (status == -1) {
         syslog(LOG_ERR, "Unable to close socket FD with error %d", errno);
     }
 
-    // Closing testfile FD
-    status = close(new_fd);
-    if (status == -1) {
-        syslog(LOG_ERR, "Unable to close new_fd FD with error %d", errno);
-    }
+    // // Closing testfile FD
+    // status = close(new_fd);
+    // if (status == -1) {
+    //     syslog(LOG_ERR, "Unable to close new_fd FD with error %d", errno);
+    // }
 
     // Closing testfile FD
     status = close(testfile_fd);
@@ -71,6 +71,30 @@ void sig_handler(int signum)
 
     closelog();
     exit(EXIT_SUCCESS);
+}
+
+int signal_initializer()
+{
+    struct sigaction act;
+    act.sa_handler = &sig_handler;
+
+    sigfillset(&act.sa_mask);
+
+    act.sa_flags = SA_RESTART;
+
+    if(sigaction(SIGINT, &act, NULL) == -1)
+	{
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+
+    if (sigaction(SIGTERM, &act, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
 
 // Function to create a daemon; based on example code given in the LSP textbook
@@ -101,11 +125,209 @@ static int daemon_create()
     return 0;
 }
 
+typedef struct {
+    pthread_t threadid;
+    int complete_flag;
+    int connection_fd;
+} thread_data_t;
+
+typedef struct node_s {
+    thread_data_t thread_data;
+    struct node_s *next;
+} node_t;
+
+// Insert element into linked list
+void ll_insert(node_t **head_ref, node_t *node)
+{
+
+}
+
+
+// Thread Function
+void* threadfunc(void* thread_param)
+{
+    thread_data_t *thread_local_vars = (thread_data_t *)thread_param;
+
+    printf("\n%d\n", thread_local_vars->connection_fd);
+
+    char *storage_buffer = (char *)malloc(INITIAL_BUFFER_SIZE);
+
+    // Static receive buffer of size 1024
+    char buffer[INITIAL_BUFFER_SIZE];
+
+    // Variable to store size of storage buffer
+    int storage_buffer_size = INITIAL_BUFFER_SIZE;
+
+    // Variable to store return value from recv
+    int bytes_received;
+
+    // Variable to store the value of the iterator within the for loop to check for "\n" character in the while loop
+    int temp_iterator = 0;
+
+    // Flag that is set when \n is received
+    int packet_complete_flag = 0;
+
+    // Variable to track the size of the storage buffer 1 indicates size is 1*1024; used when computing bytes to be written and memcpy operation on the storage buffer
+    int storage_buffer_size_count = 1;
+
+    // Variable that stores number of bytes to be written to the local file after completion of a data packet
+    int bytes_to_write = 0;
+
+    // Variable that stores bytes written to file and is used to read from the file
+    int file_size = 0;
+
+    int previous_byte_count = 0;
+
+    // While loop until bytes received is <= 0
+    while ((bytes_received = recv(thread_local_vars->connection_fd, buffer, sizeof(buffer), 0)) > 0 ) {
+
+        printf("Bytes Received = %d\n", bytes_received);
+
+        // Edge case
+        if (previous_byte_count + bytes_received > storage_buffer_size) {
+            char *temp_ptr  = (char *)realloc(storage_buffer, (storage_buffer_size + INITIAL_BUFFER_SIZE));
+            if (temp_ptr) {
+                storage_buffer = temp_ptr;
+                storage_buffer_size += INITIAL_BUFFER_SIZE;
+                storage_buffer_size_count += 1;
+            } else {
+                printf("Unable to allocate memory\n");
+            }
+        }
+
+        int i;
+        // Iterating over bytes received to check for "\n" character
+        for (i = 0; i < bytes_received; i++) {
+            if (buffer[i] == '\n') {
+                packet_complete_flag = 1;
+                temp_iterator = i+1;
+                break;
+            }
+        }
+
+        // Copying from receive buffer to storage buffer 
+        memcpy(storage_buffer + (storage_buffer_size_count - 1)*INITIAL_BUFFER_SIZE + previous_byte_count, buffer, bytes_received);
+
+        // Check if data packet is complete and assign bytes to write based on temporary iterator
+        if (packet_complete_flag == 1) {
+            bytes_to_write = (storage_buffer_size_count - 1)*INITIAL_BUFFER_SIZE + temp_iterator + previous_byte_count;
+            previous_byte_count = 0;
+            break;
+        } else {
+
+            if ((i == bytes_received) && (bytes_received < INITIAL_BUFFER_SIZE)) {
+                previous_byte_count += bytes_received;
+            }
+
+            else if (bytes_received == INITIAL_BUFFER_SIZE){
+                // Increasing size of storage buffer by INITIAL_BUFFER_SIZE amount if no "\n" character found in buffer
+                char *temp_ptr  = (char *)realloc(storage_buffer, (storage_buffer_size + INITIAL_BUFFER_SIZE));
+                if (temp_ptr) {
+                    storage_buffer = temp_ptr;
+                    storage_buffer_size += INITIAL_BUFFER_SIZE;
+                    storage_buffer_size_count += 1;
+                } else {
+                    printf("Unable to allocate memory\n");
+                }
+            }
+        }
+    }
+
+    if (bytes_received == -1) {
+        printf("Error in receive; errno is %d\n", errno);
+    }
+
+    printf("Storage Buffer size = %d\n", bytes_to_write);
+
+    // Check if packet_complete; indicating "\n" encountered in received data
+    if (packet_complete_flag == 1) {
+
+        packet_complete_flag = 0;
+
+        pthread_mutex_lock(&mutex);
+        //Writing to file
+        int nr = write(testfile_fd, storage_buffer, bytes_to_write);
+        file_size += nr; // Adding to current file length
+
+        lseek(testfile_fd, 0, SEEK_SET); // Setting the FD to start of file
+
+        // TEST
+        char *read_buffer = NULL;
+
+        int read_buffer_size;
+
+        if (file_size < WRITE_BUFFER_SIZE) {
+            read_buffer = (char *)malloc(file_size);
+            read_buffer_size = file_size;
+        } else {
+            read_buffer = (char *)malloc(WRITE_BUFFER_SIZE);
+            read_buffer_size = WRITE_BUFFER_SIZE;
+        }
+
+        if (read_buffer == NULL) {
+            printf("Unable to allocate memory to read_buffer\n");
+        }
+
+        ssize_t bytes_read;
+        int bytes_sent;
+
+        while ((bytes_read = read(testfile_fd, read_buffer, read_buffer_size)) > 0) {
+            // bytes_sent is return value from send function
+            bytes_sent = send(thread_local_vars->connection_fd, read_buffer, bytes_read, 0);
+
+            if (bytes_sent == -1) {
+                printf("Error in sending to socket!\n");
+            }
+        }
+
+        pthread_mutex_unlock(&mutex);
+        // Reading into read_buffer
+        if (bytes_read == -1)
+            perror("read");
+
+        free(read_buffer);
+
+        // ENDTEST
+
+        // Cleanup and reallocation of buffer to original size
+        free(storage_buffer);
+    }
+    thread_local_vars->complete_flag = 1;
+
+    return thread_param;
+
+}
+
+void alarm_handler(int signo)
+{
+    printf("10 seconds have passed\n");
+}
+
+int start_timer()
+{
+    struct itimerval delay;
+    int ret;
+    signal (SIGALRM, alarm_handler);
+    delay.it_value.tv_sec = 5;
+    delay.it_value.tv_usec = 0;
+    delay.it_interval.tv_sec = 10;
+    delay.it_interval.tv_usec = 0;
+    ret = setitimer (ITIMER_REAL, &delay, NULL);
+    if (ret) {
+        perror ("setitimer");
+        return -1;
+    }
+    return 0;
+}
+
 // Main function
 int main(int argc, char **argv)
 {
     // Opening Log to use syslog
     openlog(NULL, 0,  LOG_USER);
+
+    // Starting timer
+    start_timer();
 
     // Flag that is set when "-d"
     int daemon_flag = 0;
@@ -119,12 +341,15 @@ int main(int argc, char **argv)
         }
     } 
 
-    int yes = 1; // USed in the setsockopt function
+    int yes = 1; // Used in the setsockopt function
 
     printf("Socket Programming 101!\n");
 
-    signal(SIGINT, sig_handler); // Register signal handler for SIGINT
-    signal(SIGTERM, sig_handler); // Register signal handler for SIGTERM
+    int sig_ret_status = signal_initializer();
+
+    if (sig_ret_status == -1) {
+        printf("Error in initializing signals\n");
+    }
 
     // Socket SETUP begin
     // Getting a socketfd
@@ -192,198 +417,45 @@ int main(int argc, char **argv)
     if (testfile_fd == -1) {
         syslog(LOG_ERR, "Error opening file with error: %d", errno);
     } 
-    
-    // Static receive buffer of size 1024
-    char buffer[INITIAL_BUFFER_SIZE];
 
-    // Dynamically allocated storage buffer to store values received from the socket
-    // storage_buffer = (char *)malloc(INITIAL_BUFFER_SIZE);
-
-    // Variable to store size of storage buffer
-    int storage_buffer_size = INITIAL_BUFFER_SIZE;
-
-    // Variable to store return value from recv
-    int bytes_received;
-
-    // Variable to store the value of the iterator within the for loop to check for "\n" character in the while loop
-    int temp_iterator = 0;
-
-    // Flag that is set when \n is received
-    int packet_complete_flag = 0;
-
-    // Variable to track the size of the storage buffer 1 indicates size is 1*1024; used when computing bytes to be written and memcpy operation on the storage buffer
-    int storage_buffer_size_count = 1;
-
-    // Variable that stores number of bytes to be written to the local file after completion of a data packet
-    int bytes_to_write = 0;
-
-    // Variable that stores bytes written to file and is used to read from the file
-    int file_size = 0;
-
+    pthread_mutex_init(&mutex, NULL);
     // String to hold the IP address of the client; used when printing logs
     char address_string[INET_ADDRSTRLEN];
 
-    // Incomplete Flag
-   // int incomplete_flag = 0;
-    int previous_byte_count = 0;
+    // node_t *head = NULL;
 
     while(1) {
 
         // Continuously restarting connections in the while(1) loop
-        new_fd = accept(socketfd, (struct sockaddr *)&test_addr, &addr_size);
+        int new_fd = accept(socketfd, (struct sockaddr *)&test_addr, &addr_size);
 
         if (new_fd == -1) {
             perror("accept");
             continue;
         } else {
-            storage_buffer = (char *)malloc(INITIAL_BUFFER_SIZE);
-            storage_buffer_size_count = 1;
-            storage_buffer_size = INITIAL_BUFFER_SIZE;
-        }
+            // Using sock_addr_in and inet_ntop function to print the IP address of the accepted client connection
+            // Reference: https://stackoverflow.com/questions/2104495/extract-ip-from-connection-that-listen-and-accept-in-socket-programming-in-linux
+            struct sockaddr_in *p = (struct sockaddr_in *)&test_addr;
+            // TODO: Malloc for address string
+            // TODO: Put syslog within thread
+            syslog(LOG_DEBUG, "Accepted connection from %s", inet_ntop(AF_INET, &p->sin_addr, address_string, sizeof(address_string)));
 
-        // Using sock_addr_in and inet_ntop function to print the IP address of the accepted client connection
-        // Reference: https://stackoverflow.com/questions/2104495/extract-ip-from-connection-that-listen-and-accept-in-socket-programming-in-linux
-        struct sockaddr_in *p = (struct sockaddr_in *)&test_addr;
-        syslog(LOG_DEBUG, "Accepted connection from %s", inet_ntop(AF_INET, &p->sin_addr, address_string, sizeof(address_string)));
+            // pthread_t thread;
+            // Malloc new node
+            node_t *new_node = (node_t *)malloc(sizeof(node_t));
+            new_node->thread_data.complete_flag = 0;
+            new_node->thread_data.connection_fd = new_fd;
 
-        // While loop until bytes received is <= 0
-        while ((bytes_received = recv(new_fd, buffer, sizeof(buffer), 0)) > 0 ) {
-
-            printf("Bytes Received = %d\n", bytes_received);
-
-            // Edge case
-            if (previous_byte_count + bytes_received > storage_buffer_size) {
-                char *temp_ptr  = (char *)realloc(storage_buffer, (storage_buffer_size + INITIAL_BUFFER_SIZE));
-                if (temp_ptr) {
-                    storage_buffer = temp_ptr;
-                    storage_buffer_size += INITIAL_BUFFER_SIZE;
-                    storage_buffer_size_count += 1;
-                } else {
-                    printf("Unable to allocate memory\n");
-                }
-            }
-
-            int i;
-            // Iterating over bytes received to check for "\n" character
-            for (i = 0; i < bytes_received; i++) {
-                if (buffer[i] == '\n') {
-                    packet_complete_flag = 1;
-                    temp_iterator = i+1;
-                    break;
-                }
-            }
-
-            // Copying from receive buffer to storage buffer 
-            memcpy(storage_buffer + (storage_buffer_size_count - 1)*INITIAL_BUFFER_SIZE + previous_byte_count, buffer, bytes_received);
-
-            // // Copying from receive buffer to storage buffer 
-            // memcpy(storage_buffer + (storage_buffer_size_count - 1)*INITIAL_BUFFER_SIZE, buffer, bytes_received);
-
-            // Check if data packet is complete and assign bytes to write based on temporary iterator
-            if (packet_complete_flag == 1) {
-                bytes_to_write = (storage_buffer_size_count - 1)*INITIAL_BUFFER_SIZE + temp_iterator + previous_byte_count;
-                previous_byte_count = 0;
-                break;
-            } else {
-
-                if ((i == bytes_received) && (bytes_received < INITIAL_BUFFER_SIZE)) {
-                    previous_byte_count += bytes_received;
-                }
-
-                else if (bytes_received == INITIAL_BUFFER_SIZE){
-                    // Increasing size of storage buffer by INITIAL_BUFFER_SIZE amount if no "\n" character found in buffer
-                    char *temp_ptr  = (char *)realloc(storage_buffer, (storage_buffer_size + INITIAL_BUFFER_SIZE));
-                    if (temp_ptr) {
-                        storage_buffer = temp_ptr;
-                        storage_buffer_size += INITIAL_BUFFER_SIZE;
-                        storage_buffer_size_count += 1;
-                    } else {
-                        printf("Unable to allocate memory\n");
-                    }
-                }
+            // Creating a thread to run threadfunc with parameters thread_param
+            int ret = pthread_create(&(new_node->thread_data.threadid), NULL, threadfunc, &(new_node->thread_data));
+            if (ret != 0) {
+                printf("Error in pthread_create with err number: %d", errno);
+            } else if (ret == 0) {
+                printf("Success in creating thread!\n");
             }
         }
 
-        printf("Storage Buffer size = %d\n", bytes_to_write);
-
-        // Check if packet_complete; indicating "\n" encountered in received data
-        if (packet_complete_flag == 1) {
-
-            packet_complete_flag = 0;
-
-            //Writing to file
-            int nr = write(testfile_fd, storage_buffer, bytes_to_write);
-            file_size += nr; // Adding to current file length
-
-            lseek(testfile_fd, 0, SEEK_SET); // Setting the FD to start of file
-
-            // if (file_size > )
-
-            // TEST
-            char *read_buffer = NULL;
-
-            int read_buffer_size;
-
-            if (file_size < WRITE_BUFFER_SIZE) {
-                read_buffer = (char *)malloc(file_size);
-                read_buffer_size = file_size;
-            } else {
-                read_buffer = (char *)malloc(WRITE_BUFFER_SIZE);
-                read_buffer_size = WRITE_BUFFER_SIZE;
-            }
-
-            if (read_buffer == NULL) {
-                printf("Unable to allocate memory to read_buffer\n");
-            }
-
-            ssize_t bytes_read;
-            int bytes_sent;
-
-            while ((bytes_read = read(testfile_fd, read_buffer, read_buffer_size)) > 0) {
-                // bytes_sent is return value from send function
-                bytes_sent = send(new_fd, read_buffer, bytes_read, 0);
-
-                if (bytes_sent == -1) {
-                    printf("Error in sending to socket!\n");
-                }
-            }
-
-            // Reading into read_buffer
-            if (bytes_read == -1)
-                perror("read");
-
-            free(read_buffer);
-
-            // ENDTEST
-
-            // char *read_buffer = (char *)malloc(file_size);
-
-            // if (read_buffer == NULL) {
-            //     printf("Unable to allocate memory to read_buffer\n");
-            // }
-
-            // // Reading into read_buffer
-            // ssize_t bytes_read = read(testfile_fd, read_buffer, file_size);
-            // if (bytes_read == -1)
-            //     perror("read");
-
-            // // bytes_sent is return value from send function
-            // int bytes_sent = send(new_fd, read_buffer, file_size, 0);
-
-            // if (bytes_sent == -1) {
-            //     printf("Error in sending to socket!\n");
-            // }
-
-            // free(read_buffer);
-
-            // Cleanup and reallocation of buffer to original size
-            free(storage_buffer);
-            // storage_buffer = realloc(storage_buffer, INITIAL_BUFFER_SIZE);
-            // storage_buffer_size_count = 1;
-            // storage_buffer_size = INITIAL_BUFFER_SIZE;
-        }
-
-        close(new_fd);
+        // close(new_fd);
         syslog(LOG_DEBUG, "Closed connection from %s", address_string);
     }
 
