0c5c9d0044e9d1469e752234fdeff5fe696e244a
GautamaGandhi <gaga4723@colorado.edu>
2022-10-15 19:58:27 -0600

    Added comments

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -16,6 +16,15 @@
 
 #include "aesd-circular-buffer.h"
 
+// Function to increment pointer
+static uint8_t nextPtr(uint8_t ptr) 
+{
+    if (ptr + 1 == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) // Wrap around if AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED number of locations reached
+        return 0;
+    else
+        return ptr + 1;
+} 
+
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
  * @param char_offset the position to search for in the buffer list, describing the zero referenced
@@ -29,30 +38,36 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
+    // Temporary offset variable to hold char_offset
     int temp_offset = char_offset;
+
+    // Variable to hold total size in bytes of all buffers present
     int total_byte_size = 0;
     for (int i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++) {
         total_byte_size += buffer->entry[i].size;
     }
 
-    if (char_offset > total_byte_size) {
+    // Check if char_offset is more than total byte size or total bytes is less than or equal to zero
+    if ((char_offset > total_byte_size) || (total_byte_size <= 0)) {
         return NULL;
     }
 
+    // Read_ptr variable to store buffer's current read pointer
     uint8_t read_ptr = buffer->out_offs;
 
     for (int i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++) {
+        // Decrement temp_offset by the size of current buffer entry at read_ptr
         temp_offset -= buffer->entry[read_ptr].size;
+        // Check if temp_offset is less than 0 to determine if corresponding byte and buffer entry are found
         if (temp_offset < 0) {
+            // Return temp_offset to original value before subtraction and store
             temp_offset += buffer->entry[read_ptr].size;
             *entry_offset_byte_rtn = temp_offset;
+            // Return corresponding buffer entry
             return &buffer->entry[read_ptr];
         }
-        read_ptr = (read_ptr + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
-        // Wrapped around
-        if (read_ptr == buffer->in_offs) {
-            break;
-        }
+        // Increment read_ptr
+        read_ptr = nextPtr(read_ptr);
     }
     /**
     * TODO: implement per description
@@ -73,17 +88,21 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     * TODO: implement per description
     */
 
-   if (buffer->full) {
-    buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
-   }
+    // Increment buffer's read pointer if the buffer->full flag is set
+    if (buffer->full) {
+        buffer->out_offs = nextPtr(buffer->out_offs);
+    }
 
-   buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
-   buffer->entry[buffer->in_offs].size = add_entry->size;
-   buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    // Adding buffer pointer and buffer size
+    buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+    buffer->entry[buffer->in_offs].size = add_entry->size;
 
-   if (buffer->in_offs == buffer->out_offs) {
+    buffer->in_offs = nextPtr(buffer->in_offs);
+
+    // buffer->full flag is set if read pointer is equal to write pointer
+    if (buffer->in_offs == buffer->out_offs) {
         buffer->full = true;
-   }
+    }
 }
 
 /**
